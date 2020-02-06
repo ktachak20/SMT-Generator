f3018cfad4c493b9dc0bf0948bbeef175446df0b
formal <formal@example.com>
2020-02-06 18:42:12 +0530

    Added timer

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int FN_smtlib_parse( const char * );
 
@@ -11,5 +12,14 @@ int main( int argc, char **argv )
     return -1;
   }
 
-  return FN_smtlib_parse( argv[1] );
+  clock_t begin = clock();
+  int ret = FN_smtlib_parse( argv[1] );
+  clock_t end = clock();
+
+  if( ret == 0 )
+  {
+      printf("Time: %f\n", (double)(end - begin)/CLOCKS_PER_SEC);
+      return ret;
+  }
+  return ret;
 }
